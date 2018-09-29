290e002f59d3e9f2c441a1ce69e7bc43efdb930d
zgracem <zgracem@users.noreply.github.com>
2018-09-29 12:01:07 -0600

    overwrite template w/ actual site

--- config.rb
+++ config.rb
@@ -1,8 +1,7 @@
-config[:site] = {}
-
-config[:site][:title] = "My Site"
-config[:site][:url]   = "https://example.org"
-config[:site][:description] = %(This is my website.)
+config[:site]         = {}
+config[:site][:title] = "Z. (Amy) Grace Moreau"
+config[:site][:url]   = "https://zgracem.github.io"
+config[:site][:description] = %(Documentation, web design, weird digital art.)
 
 config[:source]       = "source"
 config[:layouts_dir]  = "layouts"
@@ -18,43 +17,20 @@ configure :server do
   activate :livereload do |config|
     config.no_swf = true
     config.livereload_css_pattern = /_.*\.(?:s[ca]ss|css)$/
-    config.livereload_css_target = "#{config[:css_dir]}/site.css"
+    config.livereload_css_target = "#{config[:css_dir]}/main.css"
   end
 end
 
-# configure :build do
-#   config[:http_prefix] = "/www"
-# end
+configure :build do
+  config[:build_dir] = ".."
+end
 
 activate :directory_indexes
 config[:relative_links] = true
 config[:trailing_slash] = false
 
-# Redcarpet Markdown
-
-markdown_options = {
-  smartypants: true,
-  # do not parse emphasis inside of words, e.g. foo_bar_baz
-  no_intra_emphasis: true,
-  # do not parse usual (indented) markdown code blocks
-  disable_indented_code_blocks: true,
-  # parse links even when they are not enclosed in <> characters
-  autolink: false,
-  # parse strikethrough, PHP-Markdown style, e.g. this is ~~good~~ bad
-  strikethrough: true,
-  # HTML blocks do not require empty lines around them
-  lax_spacing: true,
-  # a space is always required between the hash at the beginning of a header
-  # (e.g. "#this is my header" would not be valid)
-  space_after_headers: true
-}
-
-config[:markdown_engine] = :redcarpet
-config[:markdown] = markdown_options
-
 # Slim
 
-Slim::Embedded.options[:markdown]   = markdown_options
 Slim::Engine.options[:format]       = :html
 Slim::Engine.options[:sort_attrs]   = false
 Slim::Engine.options[:pretty]       = false
@@ -74,6 +50,8 @@ end
 
 # Autoprefixer
 
-activate :autoprefixer do |config|
-  config.browsers = ["last 3 versions", "Explorer >= 9", "> 5%"]
+configure :production do
+  activate :autoprefixer do |config|
+    config.browsers = ["last 3 versions", "Explorer >= 9", "> 5%"]
+  end
 end
